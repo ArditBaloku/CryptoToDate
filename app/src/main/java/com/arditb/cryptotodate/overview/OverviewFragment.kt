79989ddfdbbe97ee6d76fc92ab87f722aa7667f7
ArditBaloku <arditbaloku@gmail.com>
2020-04-09 12:14:34 +0200

    OverviewFragment linked with adapter

--- app/src/main/java/com/arditb/cryptotodate/overview/OverviewFragment.kt
+++ app/src/main/java/com/arditb/cryptotodate/overview/OverviewFragment.kt
@@ -6,10 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 
 
 import com.arditb.cryptotodate.R
 import com.arditb.cryptotodate.databinding.FragmentOverviewBinding
+import com.arditb.cryptotodate.network.CryptoItem
 
 
 /**
@@ -19,11 +22,22 @@ import com.arditb.cryptotodate.databinding.FragmentOverviewBinding
  */
 class OverviewFragment : Fragment() {
 
+    private val viewModel: OverviewViewModel by lazy {
+        ViewModelProviders.of(this).get(OverviewViewModel::class.java)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
         val binding = FragmentOverviewBinding.inflate(inflater)
-
         binding.setLifecycleOwner(this)
+
+        val adapter = CryptoItemAdapter()
+        binding.cryptoItemList.adapter = adapter
+
+        viewModel.properties.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.data = it
+            }
+        })
 
         return binding.root
     }
