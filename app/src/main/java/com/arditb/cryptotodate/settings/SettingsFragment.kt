bf1f319052c3b95703cfa2414ffe2534d7555820
Fatbardh <fatbardh.kadriu@gmail.com>
2020-04-10 12:29:44 +0200

    Getting value from spinners

--- app/src/main/java/com/arditb/cryptotodate/settings/SettingsFragment.kt
+++ app/src/main/java/com/arditb/cryptotodate/settings/SettingsFragment.kt
@@ -4,15 +4,18 @@ import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 
 import com.arditb.cryptotodate.R
+import kotlinx.android.synthetic.main.fragment_settings.*
 import kotlinx.android.synthetic.main.fragment_settings.view.*
+import kotlinx.android.synthetic.main.fragment_settings.view.currenciesSpinner
 
-class SettingsFragment : Fragment() {
-
+class SettingsFragment : Fragment(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +36,17 @@ class SettingsFragment : Fragment() {
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                 currenciesSpinner.adapter = adapter
             }
+
+        currenciesSpinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                resultSpinner.text = currenciesSpinner.getItemAtPosition(0).toString()
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                resultSpinner.text = currenciesSpinner.getItemAtPosition(position).toString()
+            }
+        }
+
         val languagesSpinner = view.languagesSpinner
         ArrayAdapter.createFromResource(context!!
             ,R.array.languages,android.R.layout.simple_spinner_item)
@@ -40,6 +54,16 @@ class SettingsFragment : Fragment() {
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                 languagesSpinner.adapter = adapter
             }
+
+        languagesSpinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                resultLanguages.text = languagesSpinner.getItemAtPosition(0).toString()
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                resultLanguages.text = languagesSpinner.getItemAtPosition(position).toString()
+            }
+        }
         return view
     }
-}
+ }
