9300e2959d0c15c73c16d793ba19ad1c4a89d849
Fatbardh <fatbardh.kadriu@gmail.com>
2020-04-10 11:22:19 +0200

    Added settings layout

---
+++ app/src/main/java/com/arditb/cryptotodate/settings/SettingsFragment.kt
@@ -0,0 +1,38 @@
+package com.arditb.cryptotodate.settings
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+
+import com.arditb.cryptotodate.R
+import kotlinx.android.synthetic.main.fragment_settings.view.*
+
+class SettingsFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+//        val spinner = view.currenciesSpinner
+//        ArrayAdapter.createFromResource(this
+//            ,R.array.currencies,android.R.layout.simple_spinner_item)
+//            .also { adapter ->
+//                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//                spinner.adapter = adapter
+//            }
+        return view
+    }
+}
