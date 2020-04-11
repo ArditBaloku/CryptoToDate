48571100ab34ee01e405538c0f677866c8ee7f16
Fatbardh <fatbardh.kadriu@gmail.com>
2020-04-11 14:11:51 +0200

    Swap button now works!

--- app/src/main/java/com/arditb/cryptotodate/converter/ConverterFragment.kt
+++ app/src/main/java/com/arditb/cryptotodate/converter/ConverterFragment.kt
@@ -5,21 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 
 import com.arditb.cryptotodate.R
 import com.arditb.cryptotodate.databinding.FragmentConverterBinding
+import com.arditb.cryptotodate.utils.getIndex
+import kotlinx.android.synthetic.main.fragment_settings.view.*
 
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ConverterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ConverterFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = FragmentConverterBinding.inflate(inflater)
 
+        val currencies_from = binding.convertFromSpinner
+        ArrayAdapter.createFromResource(context!!
+            ,R.array.crypto_currencies,android.R.layout.simple_spinner_item)
+            .also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                currencies_from.adapter = adapter
+            }
+
+        val currencies_to = binding.convertToSpinner
+        ArrayAdapter.createFromResource(context!!
+            ,R.array.crypto_currencies,android.R.layout.simple_spinner_item)
+            .also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                currencies_to.adapter = adapter
+            }
+        binding.swapCurrencies.setOnClickListener{
+            val first = currencies_from.selectedItem.toString()
+            currencies_from.setSelection(getIndex(currencies_from, currencies_to.selectedItem.toString()))
+            currencies_to.setSelection(getIndex(currencies_to, first))
+        }
         return binding.root
     }
+
 }
