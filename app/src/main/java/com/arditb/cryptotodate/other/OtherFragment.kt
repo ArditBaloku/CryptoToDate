4729b5f044080a8062828fdf1d93b33b955c2687
Bardhi <fatbardh.kadriu@gmail.com>
2020-04-15 13:04:15 +0200

    Added light sensor

--- app/src/main/java/com/arditb/cryptotodate/other/OtherFragment.kt
+++ app/src/main/java/com/arditb/cryptotodate/other/OtherFragment.kt
@@ -1,6 +1,10 @@
 package com.arditb.cryptotodate.other
 
 import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Build
 import android.os.Bundle
 import android.os.VibrationEffect
@@ -10,16 +14,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat.getSystemService
 
 import com.arditb.cryptotodate.R
+import kotlinx.android.synthetic.main.fragment_other.*
 import kotlinx.android.synthetic.main.fragment_other.view.*
+import java.io.IOException
 
 
-class OtherFragment : Fragment() {
+class OtherFragment : Fragment(), SensorEventListener {
 
+    var sensor: Sensor ? = null
+    var sensorManager: SensorManager? = null
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        try
+        {
+            if(event!!.values[0] < 30 )
+            {
+                display_img.visibility = View.VISIBLE
+            }
+            else
+            {
+                display_img.visibility = View.INVISIBLE
+            }
+        }catch (e: IOException)
+        {
+
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         arguments?.let {
 
         }
@@ -31,6 +62,10 @@ class OtherFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_other, container, false)
+        view.display_img.visibility = View.INVISIBLE
+
+        sensorManager = activity!!.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_LIGHT)
         val vibrator:Vibrator = activity?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         var effect: VibrationEffect = VibrationEffect.createOneShot(350, VibrationEffect.DEFAULT_AMPLITUDE  )
         view.btnVibrate.setOnClickListener{
@@ -39,6 +74,17 @@ class OtherFragment : Fragment() {
 
         }
         return view
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager!!.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager!!.unregisterListener(this)
     }
 
 }
