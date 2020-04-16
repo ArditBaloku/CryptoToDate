e9df201983f7f0e87ad2638851cb101eebf0bac0
ArditBaloku <arditbaloku@gmail.com>
2020-04-16 18:33:19 +0200

    Navigation tests now work

--- app/src/androidTest/java/com/arditb/cryptotodate/NavigationTest.kt
+++ app/src/androidTest/java/com/arditb/cryptotodate/NavigationTest.kt
@@ -1,6 +1,8 @@
 package com.arditb.cryptotodate
 
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -22,30 +24,51 @@ class NavigationTest {
     // Check if pressing the navigation button to go to the convert fragment takes you there
     @Test
     fun navigateToConvertFragment() {
-//        onView(withId(R.id.convert_btn)).perform(click()
-//        onView(withId(R.id.convert_view)).check(matches(isDisplayed()))
+        onView(withId(R.id.converterFragment)).perform(click())
+        onView(withId(R.id.converter_layout)).check(matches(isDisplayed()))
     }
 
     // Check if going to convert fragment and back to overview works
     @Test
     fun navigateToConvertAndBack() {
-//        onView(withId(R.id.convert_btn)).perform(click())
-//        onView(withId(R.id.convert_view)).check(matches(isDisplayed()))
-//        onView(withId(R.id.overview_btn)).perform(click())
-//        onView(withId(R.id.overview_view)).check(matches(isDisplayed()))
+        onView(withId(R.id.converterFragment)).perform(click())
+        onView(withId(R.id.converter_layout)).check(matches(isDisplayed()))
+        // First back is to remove the keyboard
+        pressBack()
+        // Second back is to go back to the previous fragment
+        pressBack()
+        onView(withId(R.id.overview_layout)).check(matches(isDisplayed()))
     }
 
     // Check if going to other fragment works
     @Test
     fun navigateToOtherFragment() {
-//        onView(withId(R.id.other_btn)).perform(click()
-//        onView(withId(R.id.other_view)).check(matches(isDisplayed()))
+        onView(withId(R.id.otherFragment)).perform(click())
+        onView(withId(R.id.other_layout)).check(matches(isDisplayed()))
+    }
+
+    // Check if going to other fragment and back works
+    @Test
+    fun navigateToOtherAndBack() {
+        onView(withId(R.id.otherFragment)).perform(click())
+        onView(withId(R.id.other_layout)).check(matches(isDisplayed()))
+        pressBack()
+        onView(withId(R.id.overview_layout)).check(matches(isDisplayed()))
     }
 
     // Check if going to settings fragment works
     @Test
     fun navigateToSettingsFragment() {
-//        onView(withId(R.id.settings_btn)).perform(click()
-//        onView(withId(R.id.settings_view)).check(matches(isDisplayed()))
+        onView(withId(R.id.settingsFragment)).perform(click())
+        onView(withId(R.id.settings_layout)).check(matches(isDisplayed()))
+    }
+
+    // Check if going to settings fragment and back to overview works
+    @Test
+    fun navigateToSettingsAndFragment() {
+        onView(withId(R.id.settingsFragment)).perform(click())
+        onView(withId(R.id.settings_layout)).check(matches(isDisplayed()))
+        pressBack()
+        onView(withId(R.id.overview_layout)).check(matches(isDisplayed()))
     }
 }
